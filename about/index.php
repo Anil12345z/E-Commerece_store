8f7b8c386ee4116d885d19edfda691861b332e2b
Anil chahar <84167081+Anil12345z@users.noreply.github.com>
2024-04-06 22:26:27 +0530

    Add files via upload

---
+++ about/index.php
@@ -0,0 +1,179 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from www.vrjmetalhub.com/about/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 20 May 2023 16:33:59 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VRJ Metal Hub</title>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../static/myapp/style.css">
+    <link rel="stylesheet" href="../static/myapp/about.css">
+    <link rel="stylesheet" href="../static/myapp/headerfooter.css">
+    <script src="../../use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+  </head>
+<body>
+    <div class="grid-container">
+        <header id="header-container">
+        <label class="menu-btn">
+            <i class="fas fa-bars"></i>
+         </label>
+            <div class="topnav head"  id="myTopnav" id="">
+              <div class="brand"><a href="../index.php"><img src= "../static/myapp/brand_logo.png" id="brand-logo"></a></div>
+                <div class = "wrap">
+                  <div class = "other">
+                   <a href="../index.php" class="abtn" id="home">Home</a>
+                    <a href="../newArrivals/index.php" class="abtn" id="new">New Arrivals</a>
+                    <a href="../offer/index.php" class="abtn" id="offers">Offers</a>
+                  </div>
+                  <div class="shifthover">
+                    
+                      <a href="../prodCat/indexf7b2.php?categories=1">Diyas &amp; Lanterns</a>
+                    
+                      <a href="../prodCat/index2d1f.php?categories=2">Urli</a>
+                    
+                      <a href="../prodCat/index34b9.php?categories=3">Home Decor</a>
+                    
+                      <a href="../prodCat/index5fcb.php?categories=4">Wall Decor</a>
+                    
+                      <a href="../prodCat/indexbbda.php?categories=5">Door Handles</a>
+                    
+                      <a href="../prodCat/indexe747.php?categories=6">Brass Door Knockers</a>
+                    
+                      <a href="../prodCat/indexc0bf.php?categories=7">Bells &amp; Chimes</a>
+                    
+                      <a href="../prodCat/index21b3.php?categories=8">Sprituality</a>
+                    
+                  </div>
+                </div>
+              </div>
+        </header>
+
+        <div class="main">
+          <h4> About Us </h4>
+         <div id="about">
+             <div id="image">
+              <img src="../static/myapp/logo.jpg" style="height:50vh;">
+             </div>
+
+             <div id="content">
+              <p>VRJ Metal Hub was established in 2020, with a thought to take handmade artefacts to every home & heart, offering a range of amazing & extensive metal artefacts. The product range includes God Statues and Idols, Wall Hangings, Decorative Diyas and Lamps, Door Accessories, Brass Animal Statues, Brass Trophy, Brass ShowPiece Items, Brass Designer Chowki and Brass Urlis. We offer a choicest assemblage of products that speak of elegance, style and superlative quality that will add grandeur to your beautiful home. We are amongst the leading Brass Wholesaler,Manufacturers, Retailer and Exporters based in Aligarh.</p>
+              <br>
+          <p>The designs and patterns are true to Indian colours and make it connect, no matter where you are sitting in the world. Our objective is to cater all your home decor, spiritual, celebrations needs, be it an occasion, a festival or any special day. So the next time, you are looking for handmade artefacts, think of us.
+</p> <br>
+          <p>Showcasing a range that is ethnic as well as innovative. The designs are crafted by a team of expert craftsmen keeping a superb blend of traditional and modern requirements. Bringing you a repertoire of the most exquisite collection of brass and other metals.</p>
+            </div>
+         </div>
+
+         <div id="privacy">
+            <div id="legal-disclaimer">
+              <h4> Legal Disclaimer </h4>
+              <p>VRJ METAL HUB always commit to provide 100% satisfaction product to the customer. So while purchasing please try to watch SELLER NAME AS VRJ METAL HUB and BRAND NAME "VRJ METAL HUB". 
+              ALL DISPUTES ARE SUBJECT TO ALIGARH JURISDICTION ONLY.</p>
+            </div>
+            <div id="policy">
+              <h4> Privacy Policy </h4>
+              <p>
+                The domain name www.vrjmetalhub.com (herein after referred to as "Website") is owned by Viraj Creation & Works, with its registered office at Chhipeti Mahavir Ganj, Aligarh-202001 (hereinafter referred to as "VRJ Metal Hub").</p>
+              <h6>VRJ Metal Hub Privacy Policy</h6>
+<p>We value the trust you place in VRJ Metal Hub. That's why we insist upon the highest standards for secure transactions and customer information privacy. Please read the following statement to learn about our information gathering and dissemination practices. Note: Our privacy policy is subject to change at any time without notice. To make sure you are aware of any changes, please review this policy periodically.</p>
+<h6>Use of Demographic and Profile Data</h6>
+<p>We use personal information to provide the services you request. To the extent we use your personal information to market to you, we will provide you the ability to opt-out of such uses. We use your personal information to resolve disputes; troubleshoot problems; help promote a safe service; collect fees owed; measure consumer interest in our products and services, inform you about online and offline offers, products, services, and updates; customize your experience; detect and protect us against error, fraud and other criminal activity; enforce our terms and conditions; and as otherwise described to you at the time of collection.
+In our efforts to continually improve our product and service offerings, we collect and analyse demographic and profile data about our users' activity on our website. We identify and use your IP address to help diagnose problems with our server, and to administer our website. Your IP address is also used to help identify you and to gather broad demographic information.
+Retention and Usage of Sensitive Personal Data or Information
+Except as reasonably required or otherwise permitted or required by applicable law or regulatory requirements, VRJ Metal Hub endeavours to retain your Sensitive Personal Data or Information only for as long as it believes is necessary to fulfil the purposes for which the Sensitive Personal Data or Information was collected (including, for the purpose of meeting any legal, administrative, accounting, regulatory or other reporting requirements or obligations). This Sensitive Personal Data or Information is safeguarded against inappropriate access and disclosure, as per this Privacy Policy.</p>
+<h6>Sharing of personal information</h6>
+<p>We may share personal information with our other corporate entities and affiliates to: help detect and prevent identity theft, fraud and other potentially illegal acts; correlate related or multiple accounts to prevent abuse of our services; and to facilitate joint or co-branded services that you request where such services are provided by more than one corporate entity. Those entities and affiliates may not market to you as a result of such sharing unless you explicitly opt-in.
+</p>
+<h6>Information Security Practices and Procedures</h6>
+<p>VRJ Metal Hub endeavours to maintain physical, technical and procedural safeguards that are appropriate to protect your Personal Information including Sensitive Personal Data or Information against loss, misuse, copying, damage or modification and unauthorized access or disclosure Our site has stringent security measures in place to protect the loss, misuse, and alteration of the information under our control. Whenever you change or access your account information, we offer the use of a secure server. Once your information is in our possession we adhere to strict security guidelines, protecting it against unauthorized access.
+</p>
+<h6>Choice/Opt-Out</h6>
+<p>
+The Website provides all users with the opportunity to opt-out of receiving non-essential (promotional, marketing-related) communications from us on behalf of our partners, and from us in general, after setting up an account
+The information contained in this website is for general information purposes only. The information is provided by vrjmetalhub.com and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
+Through this website you are able to link to other websites which are not under the control of. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
+Every effort is made to keep the website up and running smoothly. However, vrjmetalhub.com takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control
+ </p>
+            </div>
+         </div>
+        </div>
+
+        <footer>
+            <div class="content">
+              <div class="left box">
+                <div class="lower">
+                  <div class="footertopic">CONTACT US</div>
+                 <div class="phone" style="margin-bottom: 4vh;"><h1 style="font-size: 1.6rem;color: #b8860b;">Phone</h1>
+                  <p>7017724173</p>
+                  <p>8439986641</p>
+                  
+                  </div>
+                  <div class="email" style="margin-bottom: 4vh;"><h1 style="font-size: 1.6rem;color: #b8860b;">Email</h1>
+                  <p>metalhub007@gmail.com</p>
+                  <p>hv@vrjmetalhub.com</p>
+                    
+                  </div>
+                  <div class="email" style="margin-bottom: 4vh;"><h1 style="font-size: 1.6rem;color: #b8860b;">Address</h1>
+                    <p>9/46 Kuncha Naya Bansh, Chhipeti Mahavir Ganj,<br> Aligarh-202001</p>
+                  </div>
+                </div>
+              </div>
+               <div class="middle box">
+                <div class="footertopic" style="font-weight:normal;">USEFUL LINKS</div>
+                  <div><a href="../index.php">Home</a></div>
+                  <div><a href="index.php">About Us</a></div>
+                  <div><a href="../contact.php">Contact Us</a></div>
+                  <div><a href="index.php#privacy">Privacy Policy</a></div>
+               </div>
+              
+              <div class="middle box">
+                <div class="footertopic">CATEGORIES</div>
+                
+                    <div><a href="../prodCat/indexf7b2.php?categories=1"> Diyas &amp; Lanterns </a></div>
+                
+                    <div><a href="../prodCat/index2d1f.php?categories=2"> Urli </a></div>
+                
+                    <div><a href="../prodCat/index34b9.php?categories=3"> Home Decor </a></div>
+                
+                    <div><a href="../prodCat/index5fcb.php?categories=4"> Wall Decor </a></div>
+                
+                    <div><a href="../prodCat/indexbbda.php?categories=5"> Door Handles </a></div>
+                
+                    <div><a href="../prodCat/indexe747.php?categories=6"> Brass Door Knockers </a></div>
+                
+                    <div><a href="../prodCat/indexc0bf.php?categories=7"> Bells &amp; Chimes </a></div>
+                
+                    <div><a href="../prodCat/index21b3.php?categories=8"> Sprituality </a></div>
+                
+                </div>
+            </div>
+            <div class="bottom">
+              <p style="color:white";>Copyright © 2021 <a href="#">VRJ Metal Hub</a></p>
+            </div>
+          </footer>
+    </div>
+    
+</body>
+<script>
+const menu = document.querySelector(".menu-btn");
+const wrap = document.querySelector(".wrap");
+
+menu.addEventListener('click', function(){
+  if(wrap.style.visibility === "hidden"){
+    wrap.style.visibility = "visible"
+  }
+  else {
+    wrap.style.visibility = "hidden"
+  }
+    
+})
+</script>
+
+<!-- Mirrored from www.vrjmetalhub.com/about/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 20 May 2023 16:34:01 GMT -->
+</html>
---
+++ desc/48/index.php
@@ -0,0 +1,165 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from www.vrjmetalhub.com/desc/48/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 20 May 2023 16:36:06 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VRJ Metal Hub</title>
+    <link href="../../cdn.jsdelivr.net/npm/bootstrap%405.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <script src="../../cdn.jsdelivr.net/npm/bootstrap%405.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../static/myapp/headerfooter.css">
+    <link rel="stylesheet" href="../../static/myapp/desc.css">
+    <script src="../../use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+  </head>
+<body>
+    <div class="grid-container">
+<header id="header-container">
+        <label class="menu-btn">
+            <i class="fas fa-bars"></i>
+         </label>
+            <div class="topnav" id="myTopnav">
+              <div class="brand"><a href="../../index.php"><img src="../../static/myapp/brand_logo.png" id="brand-logo"></a></div>
+
+              <div class = "wrap">
+                  <div class = "other">
+                   <a href="../../index.php" class="abtn" id="home">Home</a>
+                    <a href="../../newArrivals/index.php" class="abtn" id="new">New Arrivals</a>
+                    <a href="../../offer/index.php" class="abtn" id="offers">Offers</a>
+                  </div>
+                
+                  <div class="shifthover">
+                    
+                      <a href="../../prodCat/indexf7b2.php?categories=1">Diyas &amp; Lanterns</a>
+                    
+                      <a href="../../prodCat/index2d1f.php?categories=2">Urli</a>
+                    
+                      <a href="../../prodCat/index34b9.php?categories=3">Home Decor</a>
+                    
+                      <a href="../../prodCat/index5fcb.php?categories=4">Wall Decor</a>
+                    
+                      <a href="../../prodCat/indexbbda.php?categories=5">Door Handles</a>
+                    
+                      <a href="../../prodCat/indexe747.php?categories=6">Brass Door Knockers</a>
+                    
+                      <a href="../../prodCat/indexc0bf.php?categories=7">Bells &amp; Chimes</a>
+                    
+                      <a href="../../prodCat/index21b3.php?categories=8">Sprituality</a>
+                    
+                  </div>
+              </div>
+            </div>
+        </header>
+       
+
+        <div class="main-container">
+          <div class="wrapper">
+            <div class="container1">
+              <img src="../../media/allProducts/Group_43.png">
+            </div>
+          </div>
+          <div class="wrapper1">
+            <div class="container2">
+              <h2>Ethnic Indian Handcrafted Brass Temple Antique Hanging Bell With Chain</h2>
+              <div class="desc-box">
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                  <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Details</button>
+                  </li>
+                  <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Description</button>
+                  </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                  <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                    <span><h4>Color:</h4> <p>Black Antique</p></span>
+                    <span><h4>Dimensions:</h4> <p>12*5.5*6 CM</p></span>
+                    <span><h4>Shipping Weight:</h4> <p>0.4 Kg</p></span>
+                    <span><h4>Material</h4> <p>Brass</p></span>
+                  </div>
+                  <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">Create a unique and peaceful ambience in your Pooja room with the beautiful Ethnic Indian Handcrafted Brass Temple Hanging Bell With Chain. Hang this elegant, fantastic and artistic bell with chain in your front door, balcony, worship room or anywhere you like.</div>
+                </div>
+    
+              </div>
+              <a href="../../contact.php"><button id="enquiry">Enquiry</button></a>
+            </div>
+          </div>
+        </div>
+
+
+    <footer>
+       <div class="content"style="background-color:white">
+          <div class="left box">
+            <div class="lower">
+              <div class="footertopic" style="font-weight:normal;">CONTACT US</div>
+<div class="phone" style="margin-bottom: 4vh;"><h1 style="font-size: 1.6rem;color: #b8860b;">Phone</h1>
+                  <p>7017724173</p>
+                  <p>8439986641</p>
+                  
+                  </div>
+                  <div class="email" style="margin-bottom: 4vh;"><h1 style="font-size: 1.6rem;color: #b8860b;">Email</h1>
+                  <p>metalhub007@gmail.com</p>
+                  <p>hv@vrjmetalhub.com</p>
+                    
+                  </div>
+                  <div class="email" style="margin-bottom: 4vh;"><h1 style="font-size: 1.6rem;color: #b8860b;">Address</h1>
+                    <p>9/46 Kuncha Naya Bansh, Chhipeti Mahavir Ganj,<br> Aligarh-202001</p>
+                  </div>
+                  </div>
+          </div>
+          <div class="middle box">
+            <div class="footertopic" style="font-weight:normal;">USEFUL LINKS</div>
+            <div><a href="../../index.php">Home</a></div>
+            <div><a href="../../about/index.php">About Us</a></div>
+            <div><a href="../../contact.php">Contact Us</a></div>
+            <div><a href="../../about/index.php#privacy">Privacy Policy</a></div>
+          </div>
+          <div class="middle box" >
+            <div class="footertopic" style="font-weight:normal;">CATEGORIES</div>
+               
+                    <div><a href="../../prodCat/indexf7b2.php?categories=1">Diyas &amp; Lanterns</a></div>
+               
+                    <div><a href="../../prodCat/index2d1f.php?categories=2">Urli</a></div>
+               
+                    <div><a href="../../prodCat/index34b9.php?categories=3">Home Decor</a></div>
+               
+                    <div><a href="../../prodCat/index5fcb.php?categories=4">Wall Decor</a></div>
+               
+                    <div><a href="../../prodCat/indexbbda.php?categories=5">Door Handles</a></div>
+               
+                    <div><a href="../../prodCat/indexe747.php?categories=6">Brass Door Knockers</a></div>
+               
+                    <div><a href="../../prodCat/indexc0bf.php?categories=7">Bells &amp; Chimes</a></div>
+               
+                    <div><a href="../../prodCat/index21b3.php?categories=8">Sprituality</a></div>
+               
+          </div>
+        </div>
+        <div class="bottom">
+         <p>Copyright © 2021 <a href="#">VRJ Metal Hub</a></p>
+        </div>
+      </footer>
+</div>
+
+</body>
+<script>
+const menu = document.querySelector(".menu-btn");
+const wrap = document.querySelector(".wrap");
+
+menu.addEventListener('click', function(){
+  if(wrap.style.visibility === "hidden"){
+    wrap.style.visibility = "visible"
+  }
+  else {
+    wrap.style.visibility = "hidden"
+  }
+    
+})
+</script>
+
+<!-- Mirrored from www.vrjmetalhub.com/desc/48/ by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 20 May 2023 16:36:06 GMT -->
+</html>
